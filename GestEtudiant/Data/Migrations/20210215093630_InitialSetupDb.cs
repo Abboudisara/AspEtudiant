b377534b553f99cdb8eef806eba075edaeff82f5
Abboudisara <57992532+Abboudisara@users.noreply.github.com>
2021-02-15 10:46:18 +0100

    Gestion

---
+++ GestEtudiant/Data/Migrations/20210215093630_InitialSetupDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestEtudiant.Data.Migrations
+{
+    public partial class InitialSetupDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jokes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    jokQuestion = table.Column<string>(nullable: true),
+                    JokAnswer = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jokes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jokes");
+        }
+    }
+}
---
+++ GestEtudiant/Models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestEtudiant.Models
+{
+    public class Jokes
+    {
+        public  int Id { get; set; }
+        public String jokQuestion { get; set; }
+        public string JokAnswer { get; set; }
+
+        public Jokes()
+        {
+
+        }
+    }
+}

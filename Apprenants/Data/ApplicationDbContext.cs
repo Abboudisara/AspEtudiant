3d4882fa9b0f8f1e9a977992888ef30c3c6f2e59
Abboudisara <57992532+Abboudisara@users.noreply.github.com>
2021-02-20 12:04:10 +0100

    gestion

---
+++ Apprenants/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Apprenants.Models;
+
+namespace Apprenants.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Apprenants.Models.Etudiants> Etudiants { get; set; }
+    }
+}
